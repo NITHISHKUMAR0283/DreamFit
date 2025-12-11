99b0d60be7a3b1264afa8392ec63566c96625f6c
NITHISHKUMAR0283 <nk0283@srmist.edu.in>
2025-12-11 13:52:28 +0530

    product  card for each product in db is added in the ui

--- dreamfit-frontend/src/pages/Dashboard.jsx
+++ dreamfit-frontend/src/pages/Dashboard.jsx
@@ -1,5 +1,5 @@
 import "../styles/Dashboard.css"
-import React,{useState} from "react"
+import React,{useEffect, useState} from "react"
 import axios from "axios"
 
 const API_BASE_URL = "http://localhost:5000/api/v1/products";
@@ -8,8 +8,25 @@ const Dashboard = () => {
     const [menuOpen,setMenuOpen] = useState(false)
     const [products, setProducts] = useState([])
     const [loading , setLoading] = useState(false)
-    const [error , setError ] = useState(null);
-    const [keyword, setKeyword] = useState("")
+    const [error , setError ] = useState(null)
+    
+    const fetch_product = async () =>{
+        setLoading(true)
+        setError("")
+        try{
+            const response =await axios.get(API_BASE_URL);
+            setProducts(response.data.products);
+        }
+        catch(error){
+            console.log(error)
+            setError("Failed to load Products from server")
+        }
+        finally{
+            setLoading(false)        }        
+    }
+    useEffect(()=>{
+        fetch_product()
+    },[])
 
     return (
         <div className="dashboard-wrapper">
@@ -35,7 +52,20 @@ const Dashboard = () => {
                 </div>
             </div>
             <hr className="line"/>
+            <div className="Product-list-container">
+                <h2>product Catelog</h2>
+                <div className="Product-grid">
+                    {products.map(product=>(
+                        <div key={product._id} className="product-cart">
+                        <h3>{product.name}</h3>
+                        <p>Price: ${product.price}</p>
+                        <img src={product?.image} alt="image"/>
+                        </div>
+                    ))}
+                </div>
+            </div>
         </div>
+        
     )
 }
 export default Dashboard
